40752c2bcb11f4b6c4d931dfb20d3b71279b484e
Thery <j.thery2311@outlook.fr>
2019-02-16 23:03:50 +0100

    add student, add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+### Ceci est mon ECF d'interface dynamique, créer chez Popschool à Lens.
+
+#### Réalisé en décembre 2018.
+
+
+Récupération d'une api sur le lien http://api-students.popschool-lens.fr/api/promotions
+
+Utilisation du Vanilla Javascript, en utilisant des fetch afin de récupérer nos promotions, et les étudiants.
+
+Je donne la possibilité via le naviguateur internet, de pouvoir ajouter/modifier/supprimer des promotions ou étudiants.
+
--- js/student.js
+++ js/student.js
@@ -1,3 +1,17 @@
+var listStudent = [];
+
+var btnAddStudent = document.querySelector('#btnAddStudent');
+var btnchangeStudent = document.querySelector('#btnchangeStudent');
+var btndeleteStudent = document.querySelector('#btndeleteStudent');
+var addStudent = document.querySelector('#addStudent');
+var selectStudent = document.querySelector('#selectStudent');
+var myDivStudent = document.querySelector('#myDivStudent');
+
+
+
+
 fetch("http://api-students.popschool-lens.fr/api/students")
     .then(response => response.json())
-    .then(function())
+    .then(function(){
+        console.log(student)
+    })
